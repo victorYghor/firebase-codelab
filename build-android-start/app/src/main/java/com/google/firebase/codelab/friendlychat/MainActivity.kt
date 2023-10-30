4f1ca832f03cc0b6ca00ed9c75739e46c533c2e8
victor <devictoryghor@gmail.com>
2023-10-29 21:29:43 -0300

    puting user variable for the app work

--- build-android-start/app/src/main/java/com/google/firebase/codelab/friendlychat/MainActivity.kt
+++ build-android-start/app/src/main/java/com/google/firebase/codelab/friendlychat/MainActivity.kt
@@ -18,14 +18,22 @@ package com.google.firebase.codelab.friendlychat
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.codelab.friendlychat.databinding.ActivityMainBinding
+import com.google.firebase.codelab.friendlychat.model.FriendlyMessage
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.BuildConfig
@@ -41,15 +49,13 @@ class MainActivity : AppCompatActivity() {
         uri?.let { onImageSelected(it) }
     }
 
+    // Firebase instance variables
     private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseDatabase
+    private lateinit var adapter: FriendlyMessageAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (BuildConfig.DEBUG) {
-            Firebase.database.useEmulator("10.0.2.2", 9000)
-            Firebase.auth.useEmulator("10.0.2.2", 9099)
-            Firebase.storage.useEmulator("10.0.2.2", 9199)
-        }
         // This codelab uses View Binding
         // See: https://developer.android.com/topic/libraries/view-binding
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -57,6 +63,7 @@ class MainActivity : AppCompatActivity() {
 
         // Initialize Firebase Auth and check if the user is signed in
         auth = Firebase.auth
+
         if(auth.currentUser == null) {
             startActivity(Intent(this, SignInActivity::class.java))
             finish()
@@ -65,14 +72,37 @@ class MainActivity : AppCompatActivity() {
         }
 
         // Initialize Realtime Database and FirebaseRecyclerAdapter
-        // TODO: implement
+        db = Firebase.database
+        val messagesRef = db.reference.child(MESSAGES_CHILD)
+        val options = FirebaseRecyclerOptions.Builder<FriendlyMessage>()
+            .setQuery(messagesRef, FriendlyMessage::class.java).build()
+        adapter = FriendlyMessageAdapter(options, getUserName())
+        binding.progressBar.visibility = ProgressBar.INVISIBLE
+        manager = LinearLayoutManager(this)
+        manager.stackFromEnd = true
+        binding.messageRecyclerView.layoutManager = manager
+        binding.messageRecyclerView.adapter = adapter
+
+        // allow scrolling in the app
+        adapter.registerAdapterDataObserver(
+            MyScrollToBottomObserver(binding.messageRecyclerView, adapter, manager)
+        )
 
         // Disable the send button when there's no text in the input field
         // See MyButtonObserver for details
         binding.messageEditText.addTextChangedListener(MyButtonObserver(binding.sendButton))
 
         // When the send button is clicked, send a text message
-        // TODO: implement
+        binding.sendButton.setOnClickListener {
+            val friendlyMessage = FriendlyMessage(
+                text = binding.messageEditText.text.toString(),
+                name = getUserName(),
+                photoUrl = getPhotoUrl(),
+                imageUrl = null
+            )
+            db.reference.child(MESSAGES_CHILD).push().setValue(friendlyMessage)
+            binding.messageEditText.setText("")
+        }
 
         // When the image button is clicked, launch the image picker
         binding.addMessageImageView.setOnClickListener {
@@ -83,15 +113,23 @@ class MainActivity : AppCompatActivity() {
     public override fun onStart() {
         super.onStart()
         // Check if user is signed in.
-        // TODO: implement
+        val user = auth.currentUser
+        if(user == null) {
+            startActivity(Intent(this, SignInActivity::class.java))
+            finish()
+            // finish the on create because this will start a new activity
+            return
+        }
     }
 
     public override fun onPause() {
+        adapter.stopListening()
         super.onPause()
     }
 
     public override fun onResume() {
         super.onResume()
+        adapter.startListening()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -111,12 +149,40 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun onImageSelected(uri: Uri) {
-        // TODO: implement
+        Log.d(TAG, "uri:$uri")
+        val tempMessage = FriendlyMessage(null, getUserName(), getPhotoUrl(), LOADING_IMAGE_URL)
+        val user = auth.currentUser
+        db.reference.child(MESSAGES_CHILD).push()
+            .setValue(tempMessage, DatabaseReference.CompletionListener { error: DatabaseError?, ref: DatabaseReference ->
+                if (error == null) {
+                    Log.w(TAG, "Database error $error")
+                    return@CompletionListener
+                }
+
+                val key = ref.key
+                val storageReference = Firebase.storage
+                    .getReference(user!!.uid)
+                    .child(key!!)
+                    .child(uri.lastPathSegment!!)
+                putImageInStorage(storageReference, uri, key)
+            })
     }
 
+    /**
+     * this put the image in the firebase storage
+     */
     private fun putImageInStorage(storageReference: StorageReference, uri: Uri, key: String?) {
         // Upload the image to Cloud Storage
-        // TODO: implement
+        storageReference.putFile(uri).addOnSuccessListener(this) { taskSnapshot ->
+            taskSnapshot.metadata!!.reference!!.downloadUrl.addOnSuccessListener { uri ->
+                // what is happing here ?
+                val friendlyMessage = FriendlyMessage(null, getUserName(), getPhotoUrl(), uri.toString())
+                db.reference.child(MESSAGES_CHILD).child(key!!).setValue(friendlyMessage)
+            }
+        }
+            .addOnFailureListener(this) { e ->
+                Log.w(TAG, "Image upload task was unsuccessful.", e)
+            }
     }
 
     private fun signOut() {
